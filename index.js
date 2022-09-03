eedb0805ef7e036eaff7bb5f59cc5c4c71fd35f6
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-02 22:32:28 -0600

    refactored code

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 let balance = 500.0;
 
-class Withdrawal {
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
     this.account = account;
-  }
-
-  commit() {
-    this.account.balance -= this.amount;
   }
 }
-class Deposit {
-  constructor(amount) {
-    this.amount = amount;
-    this.account = account;
+
+class Deposit extends Transaction {
+  commit() {
+    this.account.balance += this.amount;
   }
+}
+
+class Withdrawal extends Transaction {
   commit() {
     this.account.balance -= this.amount;
   }
@@ -30,15 +29,16 @@ class Account {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account("snow-patrol");
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
+
 console.log("Transaction 1:", t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log("Transaction 2:", t2);
 
 console.log("Balance:", balance);
-t3 = new Deposit(120.0);
+t3 = new Deposit(120.0, myAccount);
 t3.commit();
 console.log("Transaction 3:", t3);
